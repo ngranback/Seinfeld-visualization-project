d5c471bf6c8ba7a25708607b7187d8e48fd09207
Jarice Marshall <jmarshall@homeaccess.com>
2021-08-26 00:09:34 -0500

    htmllll

--- test.js
+++ test.js
@@ -1122,3 +1122,30 @@ var layout = {
 var data = [trace1, trace2, trace3, trace4, trace5, trace6];
 
 Plotly.newPlot('Chart5', data, layout);
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
